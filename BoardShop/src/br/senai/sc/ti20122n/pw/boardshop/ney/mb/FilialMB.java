5e6b10d443196f2ece3cd6b07523c761e1a6dcfd
Ney <Ney@192.168.0.103>
2014-06-26 10:13:18 -0300

    Inclusão do CRUD de filial, edição da lista de pranchas com as imagens, inclusão de cadastrar e listar filial

--- BoardShop/src/br/senai/sc/ti20122n/pw/boardshop/ney/mb/FilialMB.java
+++ BoardShop/src/br/senai/sc/ti20122n/pw/boardshop/ney/mb/FilialMB.java
@@ -1,5 +1,6 @@
 package br.senai.sc.ti20122n.pw.boardshop.ney.mb;
 
+import java.io.IOException;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
@@ -8,11 +9,14 @@ import javax.persistence.EntityManager;
 import javax.persistence.Query;
 
 import br.senai.sc.ti20122n.pw.boardshop.ney.model.entity.Filial;
+import br.senai.sc.ti20122n.pw.boardshop.ney.model.entity.Prancha;
 import br.senai.sc.ti20122n.pw.boardshop.ney.util.FacesContextUtil;
+import br.senai.sc.ti20122n.pw.boardshop.ney.util.ImagemUtil;
 
 @ManagedBean
 public class FilialMB {
 private List<Filial> filiais;
+private Filial filial;
 private EntityManager entityManager;
 
 @PostConstruct
@@ -23,7 +27,7 @@ public void init() {
 public List<Filial> getFiliais() {
 	if (filiais == null) {
 		Query query = entityManager.createQuery(
-				"select filial from Filial filial",Filial.class);
+				"select f from Filial f",Filial.class);
 		filiais = query.getResultList();
 	}
 	return filiais;
@@ -33,7 +37,32 @@ public void setFiliais(List<Filial> filiais) {
 	this.filiais = filiais;
 }
 
+public Filial getFilial() {
+	return filial;
+}
+
+public void setFilial(Filial filial) {
+	this.filial = filial;
+}
+
 public Filial buscarFilialPorId(Long id) {
 	return entityManager.find(Filial.class, id);
+}
+
+public String salvar() throws IOException {
+	entityManager.merge(getFilial());
+	return "filiallista";
+}
+
+public String editar(Long id) {
+	setFilial(entityManager.find(Filial.class, id));
+	return "filialform";
+}
+
+public String excluir(Long id) {		
+	Filial filial = entityManager.find(Filial.class, id);
+	entityManager.remove(filial);
+	filiais = null;
+	return "filiallista";
 }
 }
